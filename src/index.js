d399e6815f289e5910eb2f737206b010511a169c
Juan Ceballos <juandavi789@hotmail.com>
2022-01-23 12:44:42 -0500

    select the active element and change the active class between the links

--- src/index.js
+++ src/index.js
@@ -6,8 +6,15 @@ export const content = document.querySelector('#content');
 const homeTab = document.querySelector('#home');
 const menuTab = document.querySelector('#menu');
 const contactTab = document.querySelector('#contact');
+const subTitle = document.querySelector('.subTitle');
+const tabLinks = document.querySelectorAll('.link');
 
 content.appendChild(home());
+
+subTitle.addEventListener('click', () => {
+  content.innerHTML = '';
+  content.appendChild(home());
+});
 
 homeTab.addEventListener('click', () => {
   content.innerHTML = '';
@@ -22,4 +29,19 @@ menuTab.addEventListener('click', () => {
 contactTab.addEventListener('click', () => {
   content.innerHTML = '';
   content.appendChild(contact());
+});
+
+const tabLinksArr = Array.from(tabLinks);
+
+let activeLink;
+tabLinksArr.some((link) =>
+  link.classList.contains('active') ? (activeLink = link) : false
+);
+
+tabLinksArr.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    activeLink.classList.remove('active');
+    e.target.classList.add('active');
+    activeLink = e.target;
+  });
 });
